245032b1061e957e2b42b973cf24dc63c196f5a8
RichardZhangCS <richardzhang1303@gmail.com>
2021-11-30 14:01:29 -0500

    Made the setup for session-based authetication

--- server/app.js
+++ server/app.js
@@ -3,21 +3,20 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
+const session = require("express-session");
+var passport = require("passport");
+var crypto = require("crypto");
+var LocalStrategy = require("passport-local").Strategy;
+const MongoStore = require("connect-mongo")(session);
 
 var indexRouter = require("./routes/index");
 var postRouter = require("./routes/posts");
 
+require("dotenv").config();
 var app = express();
 
 var cors = require("cors");
-
-var mongoose = require("mongoose");
-var dev_db_url =
-  "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.czp9v.mongodb.net/tutorial-bundler?retryWrites=true&w=majority";
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(cors());
 
@@ -25,6 +24,67 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+var dev_db_url =
+  "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.czp9v.mongodb.net/tutorial-bundler?retryWrites=true&w=majority";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+passport.use(
+  new LocalStrategy(async function (username, password, cb) {
+    try {
+      var user = await User.findOne({ username: username });
+      if (!user) {
+        return cb(null, false);
+      }
+
+      const isValid = validPassword(password, user.hash, user.salt);
+
+      if (isValid) {
+        return cb(null, user);
+      } else {
+        return cb(null, false);
+      }
+    } catch (err) {
+      cb(err);
+    }
+  })
+);
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+const sessionStore = new MongoStore({
+  mongooseConnection: mongoose.connection,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: "cats",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 30,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/post", postRouter);
 app.use("/", indexRouter);
@@ -44,5 +104,24 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send(err.message);
 });
+
+function validPassword(password, hash, salt) {
+  var hashVerify = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+  return hash === hashVerify;
+}
+
+function genPassword(password) {
+  var salt = crypto.randomBytes(32).toString("hex");
+  var genHash = crypto
+    .pbkdf2Sync(password, salt, 10000, 64, "sha512")
+    .toString("hex");
+
+  return {
+    salt: salt,
+    hash: genHash,
+  };
+}
 
 module.exports = app;
